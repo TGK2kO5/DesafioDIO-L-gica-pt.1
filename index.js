8c7e13029c95dd9ca965fe1839f2a1c5b8681d3b
TGK2k05 <servicosisraelaguiar@gmail.com>
2023-12-24 12:53:22 -0300

    código finalizado

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello world!")
+let hero = "Israel"
+let xp = 10002git 
+let rank = ""
+
+if(xp <= 1000 && xp >= 0){
+    rank = "Ferro"
+}else if(xp <= 2000 && xp > 1000){
+    rank = "Bronze"
+}else if(xp <= 5000 && xp > 2000){
+    rank = "Prata"
+}else if(xp <= 7000 && xp > 5000){
+    rank = "Ouro"
+}else if(xp <= 8000 && xp > 7000){
+    rank = "Platina"
+}else if(xp <= 9000 && xp > 8000){
+    rank = "Ascendente"
+}else if(xp <= 10000 && xp > 9000){
+    rank = "Imortal"
+}else {
+    rank = "Radiante"
+}
+console.log("O herói " + hero + " está no rank " + rank)
